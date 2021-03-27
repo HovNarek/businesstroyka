8544b811e3f60879e8e0794ea7d5bd274b0cbb5a
HovNarek <narek-96@list.ru>
2021-03-27 14:39:32 +0400

    2-th commit, Adminlte

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -19,7 +19,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin');
+        return view('admin.main.index');
     }
 
     /**
---
+++ resources/views/admin/main/index.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container-fluid">
+
+    </div><!-- /.container-fluid -->
+@endsection
